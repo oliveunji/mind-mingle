71b69c6cd025fe7ce0dc3a5b9058cae78c634846
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2024-12-14 23:41:09 +0900

    feat: create ChatPage

--- mind-mingle-web/src/App.jsx
+++ mind-mingle-web/src/App.jsx
@@ -3,6 +3,7 @@ import LogoPage from './components/RegisterPage';
 import RegisterPage from './components/RegisterPage';
 import VoicePage from './components/VoicePage';
 import MainPage from './components/MainPage';
+import ChatPage from './components/ChatPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<LogoPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/voice" element={<VoicePage />} />
-
+        <Route path="/chat" element={<ChatPage />} />
         {/* <Route path="/register" element={<RegisterPage />} /> */}
       </Routes>
     </BrowserRouter>
---
+++ mind-mingle-web/src/components/ChatPage.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const ChatPage = () => {
+  
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default ChatPage;

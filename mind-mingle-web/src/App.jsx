2b65eead53eef65605ba1a339e33047748319670
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2024-12-15 11:11:13 +0900

    feat: add survey pages

--- mind-mingle-web/src/App.jsx
+++ mind-mingle-web/src/App.jsx
@@ -5,6 +5,11 @@ import VoicePage from './components/VoicePage';
 import MainPage from './components/MainPage';
 import ChatPage from './components/ChatPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Step1Page from './components/survey/Step1Page';
+import Step2Page from './components/survey/Step2Page';
+import Step3Page from './components/survey/Step3Page';
+import Step4Page from './components/survey/Step4Page';
+import Step5Page from './components/survey/Step5Page';
 
 function App() {
   return (
@@ -14,6 +19,11 @@ function App() {
         <Route path="/main" element={<MainPage />} />
         <Route path="/voice" element={<VoicePage />} />
         <Route path="/chat" element={<ChatPage />} />
+        <Route path="/survey/step1" element={<Step1Page />} />
+        <Route path="/survey/step2" element={<Step2Page />} />
+        <Route path="/survey/step3" element={<Step3Page />} />
+        <Route path="/survey/step4" element={<Step4Page />} />
+        <Route path="/survey/step5" element={<Step5Page />} />
         {/* <Route path="/register" element={<RegisterPage />} /> */}
       </Routes>
     </BrowserRouter>
---
+++ mind-mingle-web/src/components/survey/Step5Page.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { LeftOutlined } from '@ant-design/icons';
+import { Progress, Radio, Space } from 'antd';
+
+const Step5Page = () => {
+    return (
+        <>
+            <div
+                style={{
+                position: "fixed", // 화면에서 고정 위치
+                top: "10px", // 화면 위쪽에서 10px
+                left: "10px", // 화면 왼쪽에서 10px
+                textAlign: "left", // 아이콘과 텍스트를 가운데 정렬
+                marginBottom: "10px",
+                padding: "10px"
+            }}
+            >
+                <div
+                    style={{
+                    display: "flex", // Flexbox 사용
+                    alignItems: "center", // 수직 가운데 정렬
+                    gap: "10px", // 아이콘과 Progress Bar 사이 간격
+                    }}
+                >
+                    <div
+                        style={{
+                        fontSize: "24px", // 아이콘 크기
+                        color: "black", // 아이콘 색상
+                        cursor: "pointer", // 클릭 가능한 커서 표시
+                        }}
+                    >
+                        <LeftOutlined />
+                    </div>
+                    <Progress percent={100} showInfo={false} style={{ flex: 1, marginLeft: "20px", marginRight: "40px" }} />
+                </div>
+                <div
+                    style={{
+                    marginTop: "20px", // 아이콘과 문구 사이 간격
+                    fontSize: "20px", // 텍스트 크기
+                    color: "#303636", // 텍스트 색상
+                    fontWeight: "bold"
+                    }}
+                >
+                    Over the past two weeks,
+                    my days have been filled with interesting things.
+                </div>
+            </div>
+            <div>
+                <Radio.Group defaultValue={3}>
+                    <Space direction="vertical" style={{ textAlign: "left" }}>
+                        <Radio value={1} style={{ fontSize: "18px" }}>Never</Radio>
+                        <Radio value={2} style={{ fontSize: "18px" }}>Rarely</Radio>
+                        <Radio value={3} style={{ fontSize: "18px" }}>Occasionally</Radio>
+                        <Radio value={4} style={{ fontSize: "18px" }}>Often</Radio>
+                        <Radio value={5} style={{ fontSize: "18px" }}>Usually</Radio>
+                        <Radio value={6} style={{ fontSize: "18px" }}>Always</Radio>
+                    </Space>
+                </Radio.Group>
+            </div>
+            <div className="bottom-button-container">
+                <button className="rounded-button">Next</button>
+            </div>
+        </>
+    )
+}
+
+export default Step5Page;
